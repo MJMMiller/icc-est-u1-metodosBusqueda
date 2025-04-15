85e023589d6ae6ffb5c77b6d3ca02da33be10195
MJMMiller <mateomiller1605@gmail.com>
2025-04-15 09:01:39 -0500

    FEATURE(UPS-003): Metodo de busqueda lineal

--- src/App.java
+++ src/App.java
@@ -1,19 +1,26 @@
 import controllers.MetodosBusqueda;
+import models.Person;
 import views.ShowConsole;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        ShowConsole showConsole = new ShowConsole();
-
-        MetodosBusqueda metodosBusqueda = new MetodosBusqueda();
-
         
-        int[] arreglo = {1, 2, 3, 4, 5};
-        int elemento = 3;
+        Person[] personas = new Person[7];
 
-        int valor = metodosBusqueda.busquedaLineal(arreglo, elemento);
-        System.out.println(valor+1);
+        personas[0] = new Person(101, "Juan");
+        personas[1] = new Person(102, "Maria");
+        personas[2] = new Person(103, "Carlos");
+        personas[3] = new Person(104, "Ana");
+        personas[4] = new Person(105, "Luis");
+        personas[5] = new Person(106, "Sofia");
+        personas[6] = new Person(107, "Pedro");
+        MetodosBusqueda mB = new MetodosBusqueda(personas);
+
+        // int[] arreglo = {1, 2, 3, 4, 5};
+        // int elemento = 3;
+
+        // int valor = mB.busquedaLineal(arreglo, elemento);
+        // System.out.println("La posicion del elemento " + elemento + " es : " + valor);
 
 
     }
--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -1,20 +1,45 @@
 package controllers;
 
+import models.Person;
 import views.ShowConsole;
 
 public class MetodosBusqueda {
 
+    private ShowConsole showConsole;
+    private Person[] people;
 
-    public MetodosBusqueda(){
-        ShowConsole showConsole = new ShowConsole();
+    public MetodosBusqueda(Person[] persons){
+        showConsole = new ShowConsole();
+        this.people = persons;
+        showPersonByCode();
     }
-    
-    public int busquedaLineal(int[] arreglo, int elemento) {
-        for (int i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] == elemento) {
+
+    // public int busquedaLineal(int[] arreglo, int elemento) {
+    //     for (int i = 0; i < arreglo.length; i++) {
+    //         if (arreglo[i] == elemento) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    public int findPersonByCode(int code){
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].getCode() == code) {
                 return i;
             }
         }
         return -1;
+    }
+
+    public void showPersonByCode(){
+        int codeToFind = showConsole.getCode();
+        int indexPerson =  findPersonByCode(codeToFind);
+        if (indexPerson >= 0 ) {
+            showConsole.showMessage("Persona encontrada");
+            showConsole.showMessage(people[indexPerson].toString());
+        }else{
+            showConsole.showMessage("Persona codigio " + codeToFind + "no encontrada");
+        }
     }
 }
